a452c6672a72eee5992e3752b51bc811b032511c
Alex Glover <alexdglover@gmail.com>
2017-01-03 17:49:59 -0600

    changes route to nobody-exists-on-purpose image

--- app.rb
+++ app.rb
@@ -4,21 +4,26 @@ require 'json'
 set :protection, :except => [:json_csrf]
 
 get '/' do
-  respond_message "hello world"
+  string_as_json_response "hello world"
 end
 
-post '/morty_quote' do
-  content_type :json
-  {
+post '/nobody-exists-on-purpose' do
+  message = {
     :response_type => "in_channel",
-    :text => params['text'],
+    # :text => params['text'],
     :attachments => [
       { :image_url => "http://cdn.smosh.com/sites/default/files/2015/12/rickmorty15.jpg" }
     ]
-  }.to_json
+  }
+  hash_as_json_response message
 end
 
-def respond_message message
+def hash_as_json_response message
+  content_type :json
+  message.to_json
+end
+
+def string_as_json_response message
   content_type :json
   { :text => message }.to_json
 end
