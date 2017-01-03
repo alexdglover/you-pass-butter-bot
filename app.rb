c5c638ac3795139eacbfa6001f0e3c5e41df4ec6
Alex Glover <alexdglover@gmail.com>
2017-01-03 16:26:06 -0600

    first slack smoke test

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ get '/' do
   respond_message "hello world"
 end
 
+get '/morty_quote/:keyword' do
+  respond_message "you looked for a morty quote with keyword #{keyword}"
+end
+
 def respond_message message
   content_type :json
   { :text => message }.to_json
 end
+
+
