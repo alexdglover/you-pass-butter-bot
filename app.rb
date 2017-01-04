27e209723535d8cc290c7dc95c54904a755db783
Alex Glover <alexdglover@gmail.com>
2017-01-04 08:45:07 -0600

    organizes code and creates some DRY functions

--- app.rb
+++ app.rb
@@ -2,19 +2,31 @@ require 'sinatra'
 require 'json'
 require 'httparty'
 
-
 set :protection, :except => [:json_csrf]
 
-class SlackOAuthHttpClient
-  include HTTParty
-  base_uri 'https://slack.com/api/oauth.access'
-end
 
 
+################################################
+# Generic routes
+################################################
 
 get '/' do
   string_as_json_response "hello world"
 end
+
+get '/oauth' do
+  response = HTTParty.post("https://slack.com/api/oauth.access?client_id=122992570306.122925378483&client_secret=8be8c881f06d9a585b7c9e43be0185e8&code=#{params['code']}")
+  responseBody = JSON.parse(response.body)
+  if responseBody['ok'] == true
+    "<h1>You've just installed the ButterPassingRobot! Let's get riggety wrecked!!!</h1>"
+  else
+    response.body
+  end
+end
+
+################################################
+# Image fetching routes
+################################################
 
 post '/nobody-exists-on-purpose' do
   message = {
@@ -27,14 +39,29 @@ post '/nobody-exists-on-purpose' do
   hash_as_json_response message
 end
 
-get '/oauth' do
-  response = HTTParty.post("https://slack.com/api/oauth.access?client_id=122992570306.122925378483&client_secret=8be8c881f06d9a585b7c9e43be0185e8&code=#{params['code']}")
-  responseBody = JSON.parse(response.body)
-  if responseBody['ok'] == true
-    "<h1>You've just installed the ButtuerPassingRobot! Let's get riggety wrecked!!!</h1>"
-  else
-    response.body
-  end
+
+post '/riggety-riggety-wrecked' do
+  # message = {
+  #   :response_type => "in_channel",
+  #   :attachments => [
+  #     { :image_url => "http://cdn.smosh.com/sites/default/files/2015/12/rickmorty15.jpg" }
+  #   ]
+  # }
+  # hash_as_json_response message
+  image_response "http://cdn.smosh.com/sites/default/files/2015/12/rickmorty15.jpg"
+end
+
+
+# Utility functions
+
+def image_response url
+  message = {
+    :response_type => "in_channel",
+    :attachments => [
+      { :image_url => url }
+    ]
+  }
+  hash_as_json_response message
 end
 
 def hash_as_json_response message
