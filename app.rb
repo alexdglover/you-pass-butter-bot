8f8c953eaeb8f7b79432d6540aa7ae4ab2128ed6
Alex Glover <alexdglover@gmail.com>
2017-01-04 08:26:29 -0600

    adds conditional to /oauth route

--- app.rb
+++ app.rb
@@ -28,24 +28,12 @@ post '/nobody-exists-on-purpose' do
 end
 
 get '/oauth' do
-  # hash_as_json_response params
-  # post to https://slack.com/api/oauth.access
-  # client_id	  Client ID of your registered Slack application.
-  # client_secret	  Client Secret of your registered Slack application.
-  # code	  The code returned by Slack in the query string parameter.
-  # options = {
-  #   body: {
-  #     pear: { # your resource
-  #       foo: '123', # your columns/data
-  #       bar: 'second',
-  #       baz: 'last thing'
-  #     }
-  #   }
-  # }
-  #
-  # pp Partay.post('/pears.xml', options)
   response = HTTParty.post("https://slack.com/api/oauth.access?client_id=122992570306.122925378483&client_secret=8be8c881f06d9a585b7c9e43be0185e8&code=#{params['code']}")
-  response.body
+  if response.body['ok'] == true
+    "<h1>You've just installed the ButtuerPassingRobot! Let's get riggety wrecked!!!</h1>"
+  else
+    response.body
+  end
 end
 
 def hash_as_json_response message
