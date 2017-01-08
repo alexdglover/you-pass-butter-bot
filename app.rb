c4d38d45a710b7e87c2d75820fe65d204f9014e1
Alex Glover <alexdglover@gmail.com>
2017-01-07 18:11:44 -0600

    removes explicit route used for demo

--- app.rb
+++ app.rb
@@ -126,9 +126,6 @@ post '/images/all' do
   string_as_json_response "Full list of images:\n  #{images}"
 end
 
-post '/you-pass-butter' do
-  image_response "https://media.giphy.com/media/Fsn4WJcqwlbtS/giphy.gif"
-end
 
 ################################################
 # Meme related routes
