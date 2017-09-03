da2f749d9e3c4b3caf2d5dd1b6cb6bc69e7f9b1f
Alex Glover <alexdglover@gmail.com>
2017-09-03 10:45:33 -0500

    obeys rubocop

--- app.rb
+++ app.rb
@@ -94,7 +94,7 @@ get '/' do
 end
 
 get '/oauth' do
-  response = HTTParty.post("https://slack.com/api/oauth.access?client_id=" \
+  response = HTTParty.post('https://slack.com/api/oauth.access?client_id=' \
     "#{ENV['SLACK_CLIENT_ID']}&client_secret=#{ENV['SLACK_CLIENT_SECRET']}" \
     "&code=#{params['code']}")
   print(response)
